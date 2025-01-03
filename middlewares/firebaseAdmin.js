eb92a487d93f9dde2f4791bae5dcba322e73a9e8
Ali Qas <139605287+AliQas-7@users.noreply.github.com>
2025-01-03 21:39:22 +0500

    first commit

--- middlewares/firebaseAdmin.js
+++ middlewares/firebaseAdmin.js
@@ -8,11 +8,11 @@ const serviceAccount = {
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
- auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2ohqe%40first-project-3c315.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com"
+ auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL.replace('@', '%40')}`,
+  universe_domain: 'googleapis.com'
 };
 
 // Initialize Firebase Admin SDK
